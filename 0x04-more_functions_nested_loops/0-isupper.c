080cb332141be2b1a623d6a8ef47d69e37b84581
gbrownee <kaluberts@gmail.com>
2022-03-17 15:42:22 -0700

    More functions and nested loop task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,5 +7,5 @@
  */
 int _isupper(int c)
 {
-	return (c >= 'A' && v <= 'Z');
+	return (c >= 'A' && c <= 'Z');
 }
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - multiplies two integers
  * @a: int to be multiplied by b
  * @b: int to be multiplied by a
- * Return: the product
+ * Return: the result of the operation
  */
 int mul(int a, int b);
 {
