b829d44099e9391b84daab482651278b902aaa89
gbrownee <99354783+gbrownee@users.noreply.github.com>
2022-10-07 14:11:37 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int help_binary(int *array, int value, size_t lo, size_t hi);
@@ -12,5 +28,8 @@ void array_print(int *array, size_t lo, size_t hi);
 int jump_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
